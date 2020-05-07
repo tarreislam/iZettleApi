6a6c4ed9c910600b5b99fae6368466767534f9b7
bill gatez <bill.gates@michelsoft.com>
2020-05-07 19:02:08 +0200

    added query filter for purchase client

--- src/Client/PurchaseClient.php
+++ src/Client/PurchaseClient.php
@@ -23,6 +23,7 @@ final class PurchaseClient
     private $client;
     private $purchaseHistoryBuilder;
     private $purchaseBuilder;
+    private $queryParams;
 
     public function __construct(
         IzettleClientInterface $client,
@@ -36,7 +37,7 @@ final class PurchaseClient
 
     public function getPurchaseHistory(): PurchaseHistory
     {
-        $json = $this->client->getJson($this->client->get(self::GET_PURCHASES));
+        $json = $this->client->getJson($this->client->get(self::GET_PURCHASES, $this->queryParams));
 
         return $this->purchaseHistoryBuilder->buildFromJson($json);
     }
@@ -52,5 +53,49 @@ final class PurchaseClient
         $json = $this->client->getJson($response);
 
         return $this->purchaseBuilder->buildFromJson($json);
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return $this
+     */
+    public function setQueryParam($key, $value)
+    {
+        $this->queryParams[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * startDate (optional)
+     * The start date (inclusive) for purchases to be retrieved from until today or endDate. By default startDate is resolved to three years back.
+     * @param string $startDate
+     * @return $this
+     */
+    public function startDate(string $startDate)
+    {
+        return $this->setQueryParam('startDate', $startDate);
+    }
+
+    /**
+     * endDate (optional)
+     * The last date (exclusive) for purchases to be retrieved until.
+     * @param string $endDate
+     * @return $this
+     */
+    public function endDate(string $endDate)
+    {
+        return $this->setQueryParam('endDate', $endDate);
+    }
+
+    /**
+     * The maximum number of records to return. Max value of limit is 1000.
+     * @param int $limit
+     * @return $this
+     */
+    public function limit(int $limit)
+    {
+        $limit = $limit > 999 ? 1000 : $limit;
+        return $this->setQueryParam('limit',  $limit);
     }
 }
